aacf37a7a946fb0fadcb4e757480a9274ef3f9a2
Mubashir Rahman <mubashirrahman503@gmail.com>
2025-09-01 15:40:59 +0500

    added remote origin files

---
+++ index.js
@@ -0,0 +1,72 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+// types
+import { typeDefs } from './schema.js';
+
+// db.js
+import db from './_db.js';
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        game(_, args) {
+            return db.games.find(game => game.id === args.id);
+        },
+        reviews() {
+            return db.reviews;
+        },
+        review(_, args) {
+            return db.reviews.find(review => review.id === args.id)
+        },
+        authors() {
+            return db.authors;
+        },
+        author(_, args) {
+            return db.authors.find(author => author.id === args.id)
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter(r => r.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter(r => a.author_id === parent.id);
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((a) => a.id == parent.author_id)
+        },
+        game(parent) {
+            return db.games.find(g => g.id == parent.game_id)
+        }
+    },
+    Mutation: {
+        addGame(_, args) {
+            const game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game);
+            return game;
+        },
+        deleteGame(_, arg) {
+            const games = db.games.filter(g => g.id !== arg.id);
+            return games;
+        },
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+const { url } = startStandaloneServer(server, {
+    listen: 8080
+});
+
+console.log(`Server Started at: http://localhost:${8080}`);
